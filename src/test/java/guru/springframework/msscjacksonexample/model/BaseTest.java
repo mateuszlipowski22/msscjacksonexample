c356de9ddafc08c4af3f8493c1c43e705ef8b40b
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-05-07 00:52:47 +0200

    adding snake and kebab case profile with json tests

--- src/test/java/guru/springframework/msscjacksonexample/model/BaseTest.java
+++ src/test/java/guru/springframework/msscjacksonexample/model/BaseTest.java
@@ -1,10 +1,16 @@
 package guru.springframework.msscjacksonexample.model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class BaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     BeerDto getDto(){
         return BeerDto.builder()
--- src/test/java/guru/springframework/msscjacksonexample/model/BeerDtoSnakeTest.java
+++ src/test/java/guru/springframework/msscjacksonexample/model/BeerDtoSnakeTest.java
@@ -3,13 +3,14 @@ package guru.springframework.msscjacksonexample.model;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @JsonTest
-class BeerDtoTest extends BaseTest{
+@ActiveProfiles("snake")
+public class BeerDtoSnakeTest extends BaseTest{
 
     @Test
     void testSerializationDto() throws JsonProcessingException {
@@ -25,19 +26,4 @@ class BeerDtoTest extends BaseTest{
         System.out.println(jsonString);
 
     }
-
-    @Test
-    void testDeserializationJson() throws JsonProcessingException {
-        String json="{\"id\":\"cbcda84f-36f0-4175-83e3-3cd7d270d95d\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2023-05-06T22:30:59.902139569\",\"lastModifiedDate\":\"2023-05-06T22:30:59.902174951\"}\n";
-
-        BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
-
-        assertEquals("BeerName", beerDto.getBeerName());
-        assertEquals("Ale", beerDto.getBeerStyle());
-        assertEquals("12.99", beerDto.getPrice().toString());
-        assertEquals(123123123123L, beerDto.getUpc());
-
-        System.out.println(beerDto);
-    }
-
 }
